c3bcfcf6ac6373a0cb6ac6ca91270633a2814f2c
LuckyMcDev <lucky.dev@myyahoo.com>
2025-09-27 13:19:53 +0200

    Remove component system and related classes
    Deleted the entire component system, including all API types, event handlers, and implementation classes. This removes support for custom components and their event-driven integration in the project.

--- src/main/java/io/github/luckymcdev/groovyengine/api/components/types/BaseComponent.java
+++
@@ -1,8 +0,0 @@
-package io.github.luckymcdev.groovyengine.api.components.types;
-
-public interface BaseComponent<T> {
-    default void onInit() {}
-    default void onUpdate() {}
-    default void onDestroy() {}
-    T getTarget();
-}
--- src/main/java/io/github/luckymcdev/groovyengine/api/components/types/ScriptComponent.java
+++
@@ -1,5 +0,0 @@
-// ScriptComponent.java - No changes needed
-package io.github.luckymcdev.groovyengine.api.components.types;
-
-public abstract class ScriptComponent implements BaseComponent<Object>{
-}
